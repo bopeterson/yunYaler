e8e4bbde86a236cfac575efeed3387d2971f793d
bopeterson <bo.peterson@gmail.com>
2014-06-16 15:29:57 +0200

    Finalise for tutorial

--- yunYaler.ino
+++ yunYaler.ino
@@ -1,5 +1,5 @@
-#define DEBUG true //NOTE: the serial monitor must be opened if debug is true
-#define RELAYDOMAIN "gsiot-q4f3-p0y9" //Change this to your relay domain
+#define DEBUG false //NOTE: the serial monitor must be opened if debug is true
+#define RELAYDOMAIN "gsiot-abcd-efgh" //Change this to your relay domain
 
 /* 
 This sketch makes it possible to control the digital outputs of the Yun connected to 
@@ -36,32 +36,32 @@ A full tutorial can be found at http://asynkronix.se/internet-of-things-with-ard
    
    start yunYaler.py with
    
-   python yunYaler.py try.yaler.net gsiot-q4f3-p0y9 nobridge
+   python yunYaler.py try.yaler.net gsiot-abcd-efgh nobridge
    
    nobridge means that there is no bridge communication between the two processors on the Yun *
    You can now connect from anywhere with any webbrowser with 
-   http://try.yaler.net/gsiot-q4f3-p0y9/arduino/led/13/1
+   http://try.yaler.net/gsiot-abcd-efgh/arduino/led/13/1
    and Yun should give some response in the ssh terminal
  7) download this sketch to the Arduino (you must have changed the relaydomain definition to your domain first). Also set the DEBUG definition to either true or false. true requires serial monitor to be opened
  8) wait until the led 13 has stopped blinkning (ca 2 minutes)
-    NOTE1: it will blink fast (5 times a second) for 1 minute, stop blinkning for 
+    NOTE1: it will blink fast (twice a second) for 1 minute, stop blinkning for 
     some seconds and start blinking slow (once a second) for another minute
     NOTE2: if debug=true there will be no delay. Instead led 13 will blink very fast 
     until the serial monitor is opened
  9) Test it!
     set D13 to HIGH (turn built in led on) with 
-    http://try.yaler.net/gsiot-q4f3-p0y9/arduino/digital/13/1
+    http://try.yaler.net/gsiot-abcd-efgh/arduino/digital/13/1
     set D13 to LOW (turn built in led off) with
-    http://try.yaler.net/gsiot-q4f3-p0y9/arduino/digital/13/0
+    http://try.yaler.net/gsiot-abcd-efgh/arduino/digital/13/0
     read D13 state with 
-    http://try.yaler.net/gsiot-q4f3-p0y9/arduino/digital/13
+    http://try.yaler.net/gsiot-abcd-efgh/arduino/digital/13
     read analog input A4 with
-    http://try.yaler.net/gsiot-q4f3-p0y9/arduino/analog/4
+    http://try.yaler.net/gsiot-abcd-efgh/arduino/analog/4
     
    
 * the command for running yunYaler.py with the bridge enabled is
 
-python yunYaler.py try.yaler.net gsiot-q4f3-p0y9 bridge 
+python yunYaler.py try.yaler.net gsiot-abcd-efgh bridge 
 
 and this is done by the runYaler function in this sketch
 
@@ -104,7 +104,7 @@ void setup() {
   //fast blink
   while((millis()-now)<pause) {
     digitalWrite(ledPin,1-digitalRead(ledPin));
-    delay(100); 
+    delay(250); 
   }
   
   frompython[0]='-';
